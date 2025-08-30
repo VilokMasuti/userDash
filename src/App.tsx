e7406bfd4d6e8413c2e066bf720cf6b944c6c07b
VilokMasuti <119694262+VilokMasuti@users.noreply.github.com>
2025-08-30 20:30:31 +0530

    Done With setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <main className="mx-auto max-w-7xl p-4  bg-black min-h-screen">
+      <header className="mb-4 flex items-center justify-between">
+        <div>
+          <h1 className="text-2xl font-semibold text-gray-900 text-balance">
+            Admin • Users
+          </h1>
+          <p className="text-sm text-gray-500"></p>
+        </div>
+      </header>
+    </main>
+  )
+}
+export default App
